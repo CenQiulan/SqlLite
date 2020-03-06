bba1645b5f8f89693dee46df6188ba9b1161bef5
lenovo <cenqiulan@foxmail.com>
2020-03-06 21:46:25 +0800

    2.0

--- app/src/main/java/com/example/addressbook/MainActivity.java
+++ app/src/main/java/com/example/addressbook/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,7 +16,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    MyHelper myhelper;
     private Button btnWr;
     private Button btnRe;
     private Button btnUp;
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText ed_email;
     private EditText ed_phone;
     private TextView Text_info;
+    MyHelper myhelper;
     View.OnClickListener listener=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -38,18 +39,22 @@ public class MainActivity extends AppCompatActivity {
                     email=ed_email.getText().toString();
                     phone=ed_phone.getText().toString();
                     db=myhelper.getWritableDatabase();
-
-                    if(isNull()||isUnique(phone)){
+                    if(name.equals("")||email.equals("")||phone.equals("")){
+                        Toast.makeText(getApplicationContext(), "invalid data!", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(isNull()||isUnique(phone)){
                         values=new ContentValues();
                         values.put("name",name);
                         values.put("email",email);
                         values.put("phone",phone);
                         db.insert("info",null,values);
                         Toast.makeText(getApplicationContext(), "write successfully", Toast.LENGTH_SHORT).show();
-                    }else{
+                    }
+                    else{
                         Toast.makeText(getApplicationContext(), "the phone number exists", Toast.LENGTH_SHORT).show();
                     }
                     db.close();
+                    clean();
                     break;
                 case R.id.btn_read:
                     db=myhelper.getReadableDatabase();
@@ -59,13 +64,14 @@ public class MainActivity extends AppCompatActivity {
                         Cursor cursor=db.query("info",null,null,null,null,null,null,null);
                         cursor.moveToFirst();
                         Text_info.setText("");
-                        Text_info.append("NAME:"+cursor.getString(2)+"\n"+
-                                "EMAIL:"+cursor.getString(3)+"\n"+
-                                "MOBILE NUMBER:"+cursor.getString(4)+"\n");
+                        Text_info.setGravity(Gravity.LEFT);
+                        Text_info.append("NAME:"+cursor.getString(1)+"\n"+
+                                "EMAIL:"+cursor.getString(2)+"\n"+
+                                "MOBILE NUMBER:"+cursor.getString(3)+"\n");
                         while(cursor.moveToNext()){
-                            Text_info.append("NAME:"+cursor.getString(2)+"\n"+
-                                    "EMAIL:"+cursor.getString(3)+"\n"+
-                                    "MOBILE NUMBER:"+cursor.getString(4)+"\n");
+                            Text_info.append("NAME:"+cursor.getString(1)+"\n"+
+                                    "EMAIL:"+cursor.getString(2)+"\n"+
+                                    "MOBILE NUMBER:"+cursor.getString(3)+"\n");
                         }
                         cursor.close();
                     }
@@ -73,16 +79,16 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case R.id.btn_update:
                     db=myhelper.getWritableDatabase();
-                    if(isNull()||isUnique(ed_phone.getText().toString())){
+                    if(!isUnique(ed_phone.getText().toString())){
                         values=new ContentValues();
                         values.put("name",ed_name.getText().toString());
                         values.put("email",ed_email.getText().toString());
-                        values.put("name",ed_phone.getText().toString());
-                        db.update("info",values,"name=?",new String[]{ed_name.getText().toString()});
+                        db.update("info",values,"phone=?",new String[]{ed_phone.getText().toString()});
                         Toast.makeText(getApplicationContext(), "update successfully", Toast.LENGTH_SHORT).show();
                     }else{
-                        Toast.makeText(getApplicationContext(), "the phone number exists", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "you should new one", Toast.LENGTH_SHORT).show();
                     }
+                    clean();
                     db.close();
                     break;
                 case R.id.btn_remove:
@@ -90,7 +96,9 @@ public class MainActivity extends AppCompatActivity {
                     db.delete("info",null,null);
                     Toast.makeText(getApplicationContext(), "delete successfully", Toast.LENGTH_SHORT).show();
                     db.close();
-                    Text_info.setText("");
+                    Text_info.setText("---------------------\n--no records--\n---------------------");
+                    Text_info.setGravity(Gravity.CENTER_HORIZONTAL);
+                    clean();
                     break;
             }
         }
@@ -108,27 +116,36 @@ public class MainActivity extends AppCompatActivity {
         ed_email=(EditText)findViewById(R.id.edt_email);
         ed_phone=(EditText)findViewById(R.id.edt_phone);
         Text_info=(TextView)findViewById(R.id.text_show);
+        myhelper=new MyHelper(this);
         btnWr.setOnClickListener(listener);
         btnRe.setOnClickListener(listener);
         btnUp.setOnClickListener(listener);
         btnRem.setOnClickListener(listener);
     }
 
+    //check same number
     private boolean isUnique(String phone){
         SQLiteDatabase db=myhelper.getWritableDatabase();
-        Cursor cursor2=db.rawQuery("select count(*)from table info where phone=="+phone,null);
-        //query("info",null,"phone=="+ed_phone.getText().toString(),null,null,null,null,null);
+        Cursor cursor2=db.query("info",null,"phone=="+ed_phone.getText().toString(),null,null,null,null,null);
         boolean result= cursor2.getCount()==0;
         cursor2.close();
-        db.close();
         return  result;
     }
+
+    //check table
     private boolean isNull(){
         SQLiteDatabase db=myhelper.getWritableDatabase();
         Cursor cursor=db.query("info",null,null,null,null,null,null,null);
         boolean result=cursor.getCount()==0;
         cursor.close();
-        db.close();
+      //  db.close();
         return result;
+    }
+
+    //clean the editText
+    private void clean(){
+        ed_name.setText("");
+        ed_email.setText("");
+        ed_phone.setText("");
     }
 }
